747e40c41d67efdadcabe7165dced2cbc557489a
hadesyi <hadesleeq@gmail.com>
2016-10-05 08:55:55 +0900

    Declarations for Primitive and String Types

---
+++ src/chapter1/recipe1_04/TestClass.java
@@ -0,0 +1,13 @@
+package chapter1.recipe1_04;
+
+/**
+ * Project: Java8Recipes
+ * FileName: TestClass
+ * Date: 2016-10-05
+ * Time: 오전 8:55
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
