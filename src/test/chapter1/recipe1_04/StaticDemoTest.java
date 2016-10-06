948d7125ce8720ebba976394408bdec03e90a2e8
hadesyi <hadesleeq@gmail.com>
2016-10-06 13:07:19 +0900

    Declarations for Primitive and String Types Test unit

---
+++ src/test/chapter1/recipe1_04/StaticDemoTest.java
@@ -0,0 +1,23 @@
+package test.chapter1.recipe1_04; 
+
+import org.junit.After;
+import org.junit.Before;
+
+/** 
+* StaticDemo Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>10월 6, 2016</pre> 
+* @version 1.0 
+*/
+public class StaticDemoTest {
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+}
+
+} 
