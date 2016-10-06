0a1aed565e5481a7813adceade8c7b830c4f89b1
hadesyi <hadesleeq@gmail.com>
2016-10-06 13:04:25 +0900

    Declarations for Primitive and String Types

---
+++ src/chapter1/recipe1_04/StaticDemo.java
@@ -0,0 +1,14 @@
+package chapter1.recipe1_04;
+
+/**
+ * Project: Java8Recipes
+ * FileName: StaticDemo
+ * Date: 2016-10-06
+ * Time: 오후 1:03
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaticDemo {
+    public static boolean oneValueForAllObjects = false;
+}
