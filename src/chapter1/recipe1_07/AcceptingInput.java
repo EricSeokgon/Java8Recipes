5301c7a2b4d207a9cbdcfa3a0663157761171107
hadesyi <hadesleeq@gmail.com>
2016-10-10 08:56:54 +0900

    make class AcceptingInput

---
+++ src/chapter1/recipe1_07/AcceptingInput.java
@@ -0,0 +1,13 @@
+package chapter1.recipe1_07;
+
+/**
+ * Project: Java8Recipes
+ * FileName: AcceptingInput
+ * Date: 2016-10-10
+ * Time: 오전 8:56
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class AcceptingInput {
+}
