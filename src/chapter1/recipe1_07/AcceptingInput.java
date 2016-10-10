1e79eb72f0969fa6e71d05db9967d290245350fc
hadesyi <hadesleeq@gmail.com>
2016-10-10 09:00:41 +0900

    Keyboard Input and Exception Handling

--- src/chapter1/recipe1_07/AcceptingInput.java
+++ src/chapter1/recipe1_07/AcceptingInput.java
@@ -1,6 +1,7 @@
 package chapter1.recipe1_07;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -15,6 +16,28 @@ import java.io.InputStreamReader;
 public class AcceptingInput {
     public static void main(String[] args) {
         BufferedReader readIn = new BufferedReader(new InputStreamReader(System.in));
-    }
 
+        String numberAsString = "";
+        long numberAsLong = 0;
+
+        boolean numberIsValid = false;
+        do {
+            /* Ask the user for a number. */
+            System.out.println("Please enter a number: ");
+            try {
+                numberAsString = readIn.readLine();
+                System.out.println("You entered " + numberAsString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        /* Convert the number into binary form. */
+            try {
+                numberAsLong = Long.parseLong(numberAsString);
+                numberIsValid = true;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Not a number! ");
+            }
+
+        } while (numberIsValid == false);
+    }
 }
