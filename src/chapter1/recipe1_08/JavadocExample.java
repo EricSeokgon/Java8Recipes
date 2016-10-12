cd7290885f11801ba973f59c127e00fb11147b26
hadesyi <hadesleeq@gmail.com>
2016-10-12 10:20:34 +0900

    Comments Made in Javadoc Form

--- src/chapter1/recipe1_08/JavadocExample.java
+++ src/chapter1/recipe1_08/JavadocExample.java
@@ -1,5 +1,7 @@
 package chapter1.recipe1_08;
 
+import java.math.BigInteger;
+
 /**
  * Project: Java8Recipes
  * FileName: JavadocExample
@@ -10,5 +12,16 @@ package chapter1.recipe1_08;
  * To change this template use File | Settings | File Templates.
  */
 public class JavadocExample {
+    public static BigInteger addNumbers(BigInteger[] nums) {
+        BigInteger result = new BigInteger("0");
+        for (BigInteger num : nums) {
+            result = result.add(num);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        BigInteger[] someValues = {BigInteger.TEN, BigInteger.ONE};
+        System.out.println(addNumbers(someValues));
+    }
 }
