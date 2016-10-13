eeb43ef9dc849222b7af05b002f8a4510fa59b3a
hadesyi <hadesleeq@gmail.com>
2016-10-13 09:10:38 +0900

    Organizing Code with Packages

---
+++ src/chapter1/recipe1_09/JuneauWidgets.java
@@ -0,0 +1,16 @@
+package chapter1.recipe1_09;
+
+/**
+ * Project: Java8Recipes
+ * FileName: JuneauWidgets
+ * Date: 2016-10-13
+ * Time: 오전 9:09
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class JuneauWidgets {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my app!");
+    }
+}
