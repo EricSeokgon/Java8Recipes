131316b9410328a876b73737c51489a85f406e55
hadesyi <hadesleeq@gmail.com>
2016-10-14 09:06:24 +0900

    Reading an Environment Variable’s Value

--- src/chapter1/recipe1_11/ReadOneEnvVariable.java
+++ src/chapter1/recipe1_11/ReadOneEnvVariable.java
@@ -10,4 +10,16 @@ package chapter1.recipe1_11;
  * To change this template use File | Settings | File Templates.
  */
 public class ReadOneEnvVariable {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String value = System.getenv(args[0]);
+            if (value != null) {
+                System.out.println(args[0].toUpperCase() + " = " + value);
+            } else {
+                System.out.println("No such environment variable exists");
+            }
+        } else {
+            System.out.println("No argument passed");
+        }
+    }
 }
