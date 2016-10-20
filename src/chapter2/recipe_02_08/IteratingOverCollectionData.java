89eaf87c414d801c93068bc3c98ef61742a530e1
hadesyi <hadesleeq@gmail.com>
2016-10-20 09:04:08 +0900

    Iterating Over a Collection of Data

---
+++ src/chapter2/recipe_02_08/IteratingOverCollectionData.java
@@ -0,0 +1,20 @@
+package chapter2.recipe_02_08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project: Java8Recipes
+ * FileName: IteratingOverCollectionData
+ * Date: 2016-10-20
+ * Time: 오전 9:02
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class IteratingOverCollectionData {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList();
+    }
+
+}
