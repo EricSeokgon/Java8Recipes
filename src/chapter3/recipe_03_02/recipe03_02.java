3d6b9da857b8d933afc2704a362852cf9f1e1f73
hadesyi <hadesleeq@gmail.com>
2016-10-27 09:04:04 +0900

    Comparing Strings

---
+++ src/chapter3/recipe_03_02/recipe03_02.java
@@ -0,0 +1,29 @@
+package chapter3.recipe_03_02;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe03_02
+ * Date: 2016-10-27
+ * Time: 오전 9:01
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe03_02 {
+    public static void main(String[] args) {
+        String one = "one";
+        String two = "two";
+
+        String var1 = "one";
+        String var2 = "two";
+
+        String pieceone = "o";
+        String piecetwo = "ne";
+
+        // Comparison is equal
+        if (one.equals(var1)) {
+            System.out.println("String one equals var1 using equals");
+        }
+    }
+}
+
