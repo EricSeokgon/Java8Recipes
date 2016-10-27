e2d59082b94b638dfbcf9e2b17a3f817dffb4817
hadesyi <hadesleeq@gmail.com>
2016-10-27 09:09:12 +0900

    Comparing Strings

--- src/chapter3/recipe_03_02/recipe03_02.java
+++ src/chapter3/recipe_03_02/recipe03_02.java
@@ -24,6 +24,45 @@ public class recipe03_02 {
         if (one.equals(var1)) {
             System.out.println("String one equals var1 using equals");
         }
+
+        //Comparison is NOT equal
+        if (one.equals(two)) {
+            System.out.println("String one equals two using equals");
+        }
+
+        //Comparison is NOT equal
+        if (two.equals(var2)) {
+            System.out.println("String two equals var2 using equals");
+        }
+
+        //Comparison is equal, but is not directly comparing string values using
+        if (one == var1) {
+            System.out.println("String onr equals var using ==");
+        }
+
+        //Comparison is equal
+        if (two.equalsIgnoreCase(var2)) {
+            System.out.println("String two equals var2 using equalsIgnoreCase");
+        }
+
+        System.out.println("Trying to use == on String that are pieced together");
+
+        String piecedTogether = pieceone + piecetwo;
+
+        // Comparison is equal
+        if (one.equals(piecedTogether)) {
+            System.out.println("The strings contain the same value using equals");
+        }
+
+        // Comparison is NOT equal using ==
+        if (one == piecedTogether) {
+            System.out.println("The string contain the same value using == ");
+        }
+
+        // Comparison is equal
+        if (one.compareTo(var1) == 0) {
+            System.out.println("One is equal to var1 using compareTo()");
+        }
     }
 }
 
