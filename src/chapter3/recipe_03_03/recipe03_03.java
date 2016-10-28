183e21ff331976c6fa16d482abb38a3bae6940dd
hadesyi <hadesleeq@gmail.com>
2016-10-28 10:09:24 +0900

    Trimming Whitespace

---
+++ src/chapter3/recipe_03_03/recipe03_03.java
@@ -0,0 +1,18 @@
+package chapter3.recipe_03_03;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe03_03
+ * Date: 2016-10-28
+ * Time: 오전 10:08
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe03_03 {
+    public static void main(String[] args) {
+        String myString = " Thsi is a String that contains whitespace.    ";
+        System.out.println(myString);
+        System.out.println(myString.trim());
+    }
+}
