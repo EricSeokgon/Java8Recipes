ad3458951a4c6d3133341f6fbd8331c2fa39b279
hadeslee <hadesleeq@gmail.com>
2016-10-29 01:51:21 +0900

    Changing the Case of a String

---
+++ src/chapter3/recipe_03_04/recipe03_04.java
@@ -0,0 +1,12 @@
+package chapter3.recipe_03_04;
+
+/**
+ * Created by hadeslee on 2016-10-29.
+ */
+public class recipe03_04 {
+    public static void main(String[] args) {
+        String str = "This String will change case.";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
