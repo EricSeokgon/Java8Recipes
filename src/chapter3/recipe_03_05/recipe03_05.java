c3a99b76844972e2628058d701ffd2c4424995fd
hadesyi <hadesleeq@gmail.com>
2016-10-31 09:04:01 +0900

    Concatenating Strings

---
+++ src/chapter3/recipe_03_05/recipe03_05.java
@@ -0,0 +1,19 @@
+package chapter3.recipe_03_05;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe03_05
+ * Date: 2016-10-31
+ * Time: 오전 9:02
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe03_05 {
+    public static void main(String[] args) {
+        String one = "Hello";
+        String two = "Java8";
+        String result = one.concat(" ".concat(two));
+        System.out.println(result);
+    }
+}
