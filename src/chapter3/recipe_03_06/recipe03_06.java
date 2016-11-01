57a951e0c2d14850835ca37e5bba1f0bf5451d92
hadesyi <hadesleeq@gmail.com>
2016-11-01 09:07:50 +0900

    Converting Strings to Numeric Values1

---
+++ src/chapter3/recipe_03_06/recipe03_06.java
@@ -0,0 +1,19 @@
+package chapter3.recipe_03_06;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe03_06
+ * Date: 2016-11-01
+ * Time: 오전 9:06
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe03_06 {
+    public static void main(String[] args) {
+        String one = "1";
+        String two = "2";
+        int result = Integer.valueOf(one) + Integer.valueOf(two);
+        System.out.println(result);
+    }
+}
