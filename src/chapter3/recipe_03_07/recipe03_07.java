1958a68b43889dc29ed93847fa87959a9bdb628b
hadesyi <hadesleeq@gmail.com>
2016-11-02 10:05:32 +0900

    Iterating Over the Characters of a String

---
+++ src/chapter3/recipe_03_07/recipe03_07.java
@@ -0,0 +1,20 @@
+package chapter3.recipe_03_07;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe03_07
+ * Date: 2016-11-02
+ * Time: 오전 10:04
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe03_07 {
+    public static void main(String[] args) {
+        String str = "Break down into chars";
+        System.out.println(str);
+        for (char chr : str.toCharArray()) {
+            System.out.println(chr);
+        }
+    }
+}
