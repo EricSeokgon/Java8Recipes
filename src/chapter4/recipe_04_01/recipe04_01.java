3bf57a6278d63ae6130cd794a34944132f67da2c
hadesyi <hadesleeq@gmail.com>
2016-11-08 09:03:59 +0900

    Rounding Float and Double Values to Integers

---
+++ src/chapter4/recipe_04_01/recipe04_01.java
@@ -0,0 +1,20 @@
+package chapter4.recipe_04_01;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_01
+ * Date: 2016-11-08
+ * Time: 오전 9:01
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_01 {
+    public static int roundFloatToInt(float mtFloat) {
+        return Math.round(mtFloat);
+    }
+
+    public static long roundDoubleToLong(double mtDouble) {
+        return Math.round(mtDouble);
+    }
+}
