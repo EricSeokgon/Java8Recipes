4f7d42d7a339aef34f0073f2481b074572bcac4c
hadeslee <hadesleeq@gmail.com>
2016-11-14 00:25:50 +0900

    Create a new instance of the Random class

---
+++ src/chapter4/recipe_04_06/recipe04_06.java
@@ -0,0 +1,14 @@
+package chapter4.recipe_04_06;
+
+import java.util.Random;
+
+/**
+ * Created by hadeslee on 2016-11-14.
+ */
+public class recipe04_06 {
+    public static void main(String[] args) {
+        //Create a new instance of the Random class
+        Random random = new Random();
+
+    }
+}
