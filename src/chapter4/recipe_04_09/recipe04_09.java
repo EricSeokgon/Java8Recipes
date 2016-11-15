1c2e92bb7b085ce282bfaf560ef6acae3a2abe60
hadesyi <hadesleeq@gmail.com>
2016-11-15 10:03:56 +0900

    Obtaining a Year-Month-Day Date Combination solution #2

--- src/chapter4/recipe_04_09/recipe04_09.java
+++ src/chapter4/recipe_04_09/recipe04_09.java
@@ -1,6 +1,7 @@
 package chapter4.recipe_04_09;
 
 import java.time.Month;
+import java.time.MonthDay;
 import java.time.YearMonth;
 
 /**
@@ -16,7 +17,12 @@ public class recipe04_09 {
     public static void main(String[] args) {
         YearMonth yearMo = YearMonth.now();
         System.out.println("Current Year and moneth: " + yearMo);
-        YearMonth specifiedDate = YearMonth.of(2016, Month.NOVEMBER);
+        YearMonth specifiedDate = YearMonth.of(2016, Month.APRIL);
         System.out.println("Specidied Month-Day: " + specifiedDate);
+
+        MonthDay monthDay = MonthDay.now();
+        System.out.println("Current month and day : " + monthDay);
+        MonthDay specifiedDate2 = MonthDay.of(Month.NOVEMBER, 14);
+        System.out.println("Specidied Month-Day: " + specifiedDate2);
     }
 }
