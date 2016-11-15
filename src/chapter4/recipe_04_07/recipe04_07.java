0003478d139ccceb90feee61157743af23423d37
hadesyi <hadesleeq@gmail.com>
2016-11-15 09:46:50 +0900

    Obtaining the Current Date Without Time

---
+++ src/chapter4/recipe_04_07/recipe04_07.java
@@ -0,0 +1,19 @@
+package chapter4.recipe_04_07;
+
+import java.time.LocalDate;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_07
+ * Date: 2016-11-15
+ * Time: 오전 9:45
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_07 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date : " + date);
+    }
+}
