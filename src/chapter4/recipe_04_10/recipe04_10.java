203737880df7048a697cb77f434a14c19b89a093
hadesyi <hadesleeq@gmail.com>
2016-11-16 09:16:21 +0900

    Obtaining and Calculating Based on the Current Time

---
+++ src/chapter4/recipe_04_10/recipe04_10.java
@@ -0,0 +1,20 @@
+package chapter4.recipe_04_10;
+
+import java.time.LocalTime;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_10
+ * Date: 2016-11-16
+ * Time: 오전 9:15
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_10 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+        System.out.println("Current Time : " + time);
+
+    }
+}
