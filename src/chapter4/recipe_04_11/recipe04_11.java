0c48a1a88dc6854b7f99d82199c468d3a5cd0c6e
hadesyi <hadesleeq@gmail.com>
2016-11-17 10:34:01 +0900

    Obtaining and Using the Date and Time Together

---
+++ src/chapter4/recipe_04_11/recipe04_11.java
@@ -0,0 +1,19 @@
+package chapter4.recipe_04_11;
+
+import java.time.LocalDateTime;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_11
+ * Date: 2016-11-17
+ * Time: 오전 10:32
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_11 {
+    public static void main(String[] args) {
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println("Locl Date and Time : " + ldt);
+    }
+}
