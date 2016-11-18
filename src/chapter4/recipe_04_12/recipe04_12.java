f8275423dd59e3bc9d9a8d510810bb124ad18def
hadeslee <hadesleeq@gmail.com>
2016-11-19 00:13:19 +0900

    Obtaining a Machine Timestamp

---
+++ src/chapter4/recipe_04_12/recipe04_12.java
@@ -0,0 +1,13 @@
+package chapter4.recipe_04_12;
+
+import java.time.Instant;
+
+/**
+ * Created by hadeslee on 2016-11-19.
+ */
+public class recipe04_12 {
+    public static void instants() {
+        Instant timestamp = Instant.now();
+        System.out.println("The current timestamp : " + timestamp);
+    }
+}
