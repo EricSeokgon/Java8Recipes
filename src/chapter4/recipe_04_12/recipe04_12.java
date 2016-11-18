755f5d36c36464291be628f51050f5af86b8627e
hadeslee <hadesleeq@gmail.com>
2016-11-19 00:15:27 +0900

    //Now minus three days

--- src/chapter4/recipe_04_12/recipe04_12.java
+++ src/chapter4/recipe_04_12/recipe04_12.java
@@ -1,6 +1,9 @@
 package chapter4.recipe_04_12;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Created by hadeslee on 2016-11-19.
@@ -9,5 +12,13 @@ public class recipe04_12 {
     public static void instants() {
         Instant timestamp = Instant.now();
         System.out.println("The current timestamp : " + timestamp);
+
+        //Now minus three days
+        Instant minusThree = timestamp.minus(3, ChronoUnit.DAYS);
+        System.out.println("Now minus three days : " + minusThree);
+
+        ZonedDateTime atZone = timestamp.atZone(ZoneId.of("GMT"));
+        System.out.println(atZone);
+
     }
 }
