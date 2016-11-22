db6fd9b94202aaa7799de6f7793ba0722c70fae1
hadesyi <hadesleeq@gmail.com>
2016-11-22 11:19:09 +0900

    Finding the Interval Between Dates and Times

---
+++ src/chapter4/recipe_04_15/recipe04_15.java
@@ -0,0 +1,25 @@
+package chapter4.recipe_04_15;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_15
+ * Date: 2016-11-22
+ * Time: 오전 11:13
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_15 {
+    public static void main(String[] args) {
+        LocalDate annicersary = LocalDate.of(2000, Month.NOVEMBER, 11);
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(annicersary, today);
+        System.out.println("Number of Day Difference : " + period.getDays());
+        System.out.println("Number of Months Difference : " + period.getMonths());
+        System.out.println("Number of Year Difference : " + period.getYears());
+    }
+}
