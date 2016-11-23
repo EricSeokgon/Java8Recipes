e5a68d4542eef4e1def4ba5498159827253c3ef9
hadesyi <hadesleeq@gmail.com>
2016-11-23 14:32:41 +0900

    //Using a different Parser

--- src/chapter4/recipe_04_16/recipe04_16.java
+++ src/chapter4/recipe_04_16/recipe04_16.java
@@ -2,6 +2,7 @@ package chapter4.recipe_04_16;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Project: Java8Recipes
@@ -19,5 +20,9 @@ public class recipe04_16 {
         LocalDateTime ldt = LocalDateTime.parse("2016-11-23T14:30:00");
         System.out.println("Parsed Date : " + ld);
         System.out.println("Parsed Date : " + ldt);
+
+        //Using a different Parser
+        LocalDate ldt2 = LocalDate.parse("2016-11-23", DateTimeFormatter.ISO_DATE);
+        System.out.println("Different Parser : " + ldt2);
     }
 }
