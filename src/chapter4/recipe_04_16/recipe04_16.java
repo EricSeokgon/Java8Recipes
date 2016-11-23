5ecf97b739fb7c06abfc366f220a964891a9b098
hadesyi <hadesleeq@gmail.com>
2016-11-23 14:31:29 +0900

    //Parse a string to form a Date-Time object

---
+++ src/chapter4/recipe_04_16/recipe04_16.java
@@ -0,0 +1,23 @@
+package chapter4.recipe_04_16;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_16
+ * Date: 2016-11-23
+ * Time: 오후 2:28
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_16 {
+    public static void main(String[] args) {
+        //Parse a string to form a Date-Time object
+        LocalDate ld = LocalDate.parse("2016-11-23");
+        LocalDateTime ldt = LocalDateTime.parse("2016-11-23T14:30:00");
+        System.out.println("Parsed Date : " + ld);
+        System.out.println("Parsed Date : " + ldt);
+    }
+}
