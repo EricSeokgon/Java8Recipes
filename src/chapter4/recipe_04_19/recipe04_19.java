3f4b6bfd749a36c482a5ca7512f4982af0f542b3
hadesyi <hadesleeq@gmail.com>
2016-11-24 14:26:25 +0900

    Declaring Binary Literals

---
+++ src/chapter4/recipe_04_19/recipe04_19.java
@@ -0,0 +1,21 @@
+package chapter4.recipe_04_19;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_19
+ * Date: 2016-11-24
+ * Time: 오후 2:26
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_19 {
+    public static void main(String[] args) {
+        int bin1 = 0b1100;
+        short bin2 = 0B010101;
+        short bin3 = (short) 0b1001100110011001;
+        System.out.println(bin1);
+        System.out.println(bin2);
+        System.out.println(bin3);
+    }
+}
