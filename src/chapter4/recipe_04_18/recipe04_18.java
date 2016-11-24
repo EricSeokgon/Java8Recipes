898b6343f45d90e4ac2b758f9ad5c9497453f049
hadesyi <hadesleeq@gmail.com>
2016-11-24 14:25:31 +0900

    Writing Readable Numeric Literals

---
+++ src/chapter4/recipe_04_18/recipe04_18.java
@@ -0,0 +1,19 @@
+package chapter4.recipe_04_18;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe04_18
+ * Date: 2016-11-24
+ * Time: 오후 2:24
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe04_18 {
+    public static void main(String[] args) {
+        int million = 1_000_000;
+        int billion = 1_000_000_000;
+        float ten_pct = 1_0f;
+        double exp = 1_234_56.78_9e2;
+    }
+}
