aefe9a14f8f00a6a8d3fa2be3f960ee63d329d96
hadesyi <hadesleeq@gmail.com>
2016-11-24 14:29:14 +0900

    Making Private Fields Accessible To Other Classes

---
+++ src/chapter5/recipe_05_02/recipe05_02.java
@@ -0,0 +1,22 @@
+package chapter5.recipe_05_02;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe05_02
+ * Date: 2016-11-24
+ * Time: 오후 2:28
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe05_02 {
+    private String firstName = null;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
