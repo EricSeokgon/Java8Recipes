9146a0e4e209b27906967b684b8ab14bdb05db7e
hadesyi <hadesleeq@gmail.com>
2016-11-24 14:27:42 +0900

    Controlling Access to Members of a Class

---
+++ src/chapter5/recipe_05_01/recipe05_01.java
@@ -0,0 +1,17 @@
+package chapter5.recipe_05_01;
+
+/**
+ * Project: Java8Recipes
+ * FileName: recipe05_01
+ * Date: 2016-11-24
+ * Time: 오후 2:27
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class recipe05_01 {
+    private String firstName = null;
+    private String lastName = null;
+    private String position = null;
+    private int status = -1;
+}
