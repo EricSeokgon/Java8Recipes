102ee30a55773c6333419dd9fe33d619bf8f2ed4
hadesyi <hadesleeq@gmail.com>
2016-11-30 09:53:03 +0900

    Generating Instances of a Class

---
+++ src/chapter5/recipe_05_04/PlayerFactory.java
@@ -0,0 +1,38 @@
+package chapter5.recipe_05_04;
+
+/**
+ * Project: Java8Recipes
+ * FileName: PlayerFactory
+ * Date: 2016-11-30
+ * Time: 오전 9:48
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlayerFactory {
+    public static Player createPlayer(String playerType) {
+        player returnType;
+        switch (playerType) {
+            case "GOALIE":
+                returnType = new Goalie();
+                break;
+            case "LEFT":
+                returnType = new LeftWing();
+                break;
+            case "RIGHT":
+                returnType = new RightWing();
+                break;
+            case "CENTER":
+                returnType = new Center();
+                break;
+            case "DEFENSE":
+                returnType = new Defense();
+                break;
+            default:
+                returnType = new AllPlayer();
+        }
+        return returnType;
+    }
+}
+
+
