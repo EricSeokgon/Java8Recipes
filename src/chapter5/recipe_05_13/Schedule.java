2398b0fd526960e0171b484b5cb914ae1a518480
hadesyi <hadesleeq@gmail.com>
2016-12-06 10:13:05 +0900

    Defining a Template for Classes to Extend

---
+++ src/chapter5/recipe_05_13/Schedule.java
@@ -0,0 +1,14 @@
+package chapter5.recipe_05_13;
+
+/**
+ * Project: Java8Recipes
+ * FileName: Schedule
+ * Date: 2016-12-06
+ * Time: 오전 10:12
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Schedule {
+
+}
