427c38bd625dd3b3df4bc8ede8e6de3956ac9897
hadesyi <hadesleeq@gmail.com>
2016-12-06 10:17:25 +0900

    Defining a Template for Classes to Extend

--- src/chapter5/recipe_05_13/Schedule.java
+++ src/chapter5/recipe_05_13/Schedule.java
@@ -1,5 +1,12 @@
 package chapter5.recipe_05_13;
 
+import chapter5.recipe_05_11.Team;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Project: Java8Recipes
  * FileName: Schedule
@@ -10,5 +17,30 @@ package chapter5.recipe_05_13;
  * To change this template use File | Settings | File Templates.
  */
 public abstract class Schedule {
+    public String scheduleYear;
+    public String teamName;
+    public List<Team> teams;
+    public List homeGames;
+    public List awayGames;
+    Map gameMap;
 
+    public Schedule() {
+    }
+
+    public Schedule(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public Map obtainSchedule() {
+        if (gameMap == null) {
+            gameMap = new HashMap();
+        }
+        return gameMap;
+    }
+
+    public void setGameDate(Team team, Date date) {
+        obtainSchedule().put(team, date)
+    }
+
+    abstract void calculateDayPlayed(int month);
 }
