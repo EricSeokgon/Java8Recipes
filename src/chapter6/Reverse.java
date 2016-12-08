385c6887137fa15c132621468ea513586bcf8459
hadeslee <hadesleeq@gmail.com>
2016-12-09 04:17:05 +0900

    Enabling the Use of Lambda Expressions 2

--- src/chapter6/Reverse.java
+++ src/chapter6/Reverse.java
@@ -1,5 +1,7 @@
 package chapter6;
 
+import java.util.function.Function;
+
 /**
  * Created by hadeslee on 2016-12-09.
  */
@@ -16,7 +18,15 @@ public class Reverse {
             }
             return tempStr;
         };
+        Function<String,String> newText2 = (testText) -> {
+            String tempStr = "";
+            for (String part : testText.split(" ")) {
+                tempStr = new StringBuilder(part).reverse().toString();
+            }
+            return tempStr;
+        };
         System.out.println(newText.reverse("HELLO"));
+        System.out.println(newText2.apply("WORLD"));
     }
 }
 
