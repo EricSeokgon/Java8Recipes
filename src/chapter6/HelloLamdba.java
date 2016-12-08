0e905caf03973e4a882fbf1c476fe86a8b011c3e
hadesyi <hadesleeq@gmail.com>
2016-12-08 16:13:15 +0900

    Writing a Simple Lambda Expression

---
+++ src/chapter6/HelloLamdba.java
@@ -0,0 +1,14 @@
+package chapter6;
+
+/**
+ * Project: Java8Recipes
+ * FileName: HelloLamdba
+ * Date: 2016-12-08
+ * Time: 오후 4:12
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloLamdba {
+
+}
