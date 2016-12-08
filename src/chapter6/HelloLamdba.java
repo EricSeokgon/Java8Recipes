dc3c62f410fb3fbc4294127bfef368402c5cafa8
hadesyi <hadesleeq@gmail.com>
2016-12-08 16:21:37 +0900

    Writing a Simple Lambda Expression

--- src/chapter6/HelloLamdba.java
+++ src/chapter6/HelloLamdba.java
@@ -14,10 +14,27 @@ public class HelloLamdba {
         void hello(String text);
     }
 
+    public interface ActionCode{
+        int returnCode(String codestr);
+    }
+
     public static void main(String[] args) {
         HelloType helloLamdba = (String text) -> {
             System.out.println("Hello " + text);
         };
+        ActionCode code =(codestr)->{
+            switch (codestr) {
+                case "ACTIVE":
+                    return 0;
+                case "INACTIVE":
+                    return 1;
+                    default:
+                        return -1;
+            }
+        };
+
         helloLamdba.hello("Lamdba");
+        code.returnCode("ACTIVE");
+
     }
 }
