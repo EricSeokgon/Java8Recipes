e49e6ce65d3036ac4192ef711dde5f7ec13eb04d
hadeslee <hadesleeq@gmail.com>
2016-12-10 03:26:24 +0900

    Accessing Class Variables from a Lambda Expression

---
+++ src/chapter6/VariableAccessInner.java
@@ -0,0 +1,24 @@
+package chapter6;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by hadeslee on 2016-12-10.
+ */
+public class VariableAccessInner {
+    public String CLASSA = "Class-level A";
+    class InnerClass{
+        public String CLASSA = "Class-level B";
+
+        void lamdbaInMethod(String passedIn) {
+            String METHODA = "Method-level A";
+            Consumer<String> l1= x->{
+                System.out.println(x);
+                System.out.println("CLASSA Value: " + CLASSA);
+                System.out.println("METHOD Value: " + METHODA);
+            };
+            l1.accept(CLASSA);
+            l1.accept(passedIn);
+        }
+    }
+}
