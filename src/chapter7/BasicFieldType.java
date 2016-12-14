e015374c7c0590899619dc169d06ea7943271fe7
hadesyi <hadesleeq@gmail.com>
2016-12-14 15:44:36 +0900

    Defining a Fixed Set of Related Constants

---
+++ src/chapter7/BasicFieldType.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+/**
+ * Project: Java8Recipes
+ * FileName: BasicFieldType
+ * Date: 2016-12-14
+ * Time: 오후 3:42
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class BasicFieldType {
+    public enum FieldType {PASSWORD, EMAIL_ADDRESS, PHONE_NUMBER, SOCIAL_SECURITY_NUMBER}
+
+}
