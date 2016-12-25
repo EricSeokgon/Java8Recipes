89f788e8e13b137bdd6eb1b42c6a8318fa47c5a5
hadeslee <hadesleeq@gmail.com>
2016-12-26 00:37:48 +0900

    Working with Dynamic Arrays

--- src/chapter7/StockScreener.java
+++ src/chapter7/StockScreener.java
@@ -1,5 +1,6 @@
 package chapter7;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -42,4 +43,24 @@ public class StockScreener {
         }
     }
 
+    public static void main(String[] args) {
+        List<String> stocks = new ArrayList<>();
+        stocks.add("ORCL");
+        stocks.add("AAPL");
+        stocks.add("GOOG");
+        stocks.add("IBM");
+        stocks.add("MCD");
+        System.out.println("Screening stocks: " + stocks);
+        if (stocks.contains("GOOG") &&
+                !screen("GOOG", Screen.BETA, 1.1)) {
+            stocks.remove("GOOG");
+        }
+        System.out.println("First screen: " + stocks);
+        StockScreener.screen(stocks, Screen.YIELD, 3.5);
+        System.out.println("Second screen: " + stocks);
+        StockScreener.screen(stocks, Screen.PE, 22);
+        System.out.println("Third screen: " + stocks);
+        System.out.println("Buy List: " + stocks);
+    }
 }
+
