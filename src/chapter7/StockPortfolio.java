12f40b59942cc56e0cf430f01933cc93a2f24566
hadesyi <hadesleeq@gmail.com>
2016-12-27 09:12:03 +0900

    Making Your Objects Iterable

--- src/chapter7/StockPortfolio.java
+++ src/chapter7/StockPortfolio.java
@@ -1,6 +1,8 @@
 package chapter7;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,7 +14,32 @@ import java.util.Map;
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-public class StockPortfolio implements Iterable<Stock>{
+public class StockPortfolio implements Iterable<Stock> {
     Map<String, Stock> portdolio = new HashMap<>();
 
+    public void add(Stock stock) {
+        portdolio.put(stock.getSymbol(), stock);
+    }
+
+    public void add(List<Stock> stocks) {
+        for (Stock s : stocks) {
+            portdolio.put(s.getSymbol(), s);
+        }
+    }
+
+    @Override
+    public Iterator<Stock> iterator() {
+        return portdolio.values().iterator();
+    }
+
+    public static void main(String[] args) {
+        StockPortfolio myPortfolio = new StockPortfolio();
+        myPortfolio.add(new Stock("ORCL", "Oracle", 500.0));
+        myPortfolio.add(new Stock("AAPL", "Apple", 200.0));
+        myPortfolio.add(new Stock("GOOG", "Google", 100.0));
+        myPortfolio.add(new Stock("IBM", "IBM", 50.0));
+        myPortfolio.add(new Stock("MCD", "McDonalds", 300.0));
+        System.out.println("======Print using legacy for-each loop=====");
+        myPortfolio.forEach(s -> System.out.println(s));
+    }
 }
