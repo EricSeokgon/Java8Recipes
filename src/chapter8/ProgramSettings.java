79d81aa97e09d055386bf4dac5431c3bc0b298ea
hadeslee <hadesleeq@gmail.com>
2016-12-29 00:35:09 +0900

    Serializing Java Objects

---
+++ src/chapter8/ProgramSettings.java
@@ -0,0 +1,12 @@
+package chapter8;
+
+import java.awt.*;
+
+/**
+ * Created by hadeslee on 2016-12-29.
+ */
+public class ProgramSettings {
+    public ProgramSettings(Point point, Dimension dimension, Color blue, String s) {
+    }
+}
+
