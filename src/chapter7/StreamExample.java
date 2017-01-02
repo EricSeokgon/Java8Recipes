d8df1ba26a766f0c45c4242860fb15538f685744
hadesyi <hadesleeq@gmail.com>
2017-01-03 08:48:01 +0900

    Iterating Over Collections

--- src/chapter7/StreamExample.java
+++ src/chapter7/StreamExample.java
@@ -1,16 +1,17 @@
 package chapter7;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 /**
- * Created by hadeslee on 2016-12-28.
+ * Project: Java8Recipes
+ * FileName: StreamExample
+ * Date: 2017-01-03
+ * Time: 오전 8:43
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
  */
-
 public class StreamExample {
     static List<Stock> myStocks = new ArrayList();
 
@@ -24,41 +25,10 @@ public class StreamExample {
 
     public static void main(String[] args) {
         createStocks();
-// Iterate over each element and print the stock names
         myStocks.stream()
                 .forEach(s -> System.out.println(s.getName()));
         boolean allGt = myStocks.stream()
                 .allMatch(s -> s.getShares() > 100.0);
-        System.out.println("All Stocks Greater Than 100.0 Shares? " + allGt);
-// Print out all stocks that have more than 100 shares
-        System.out.println("== We have more than 100 shares of the following:");
-        myStocks.stream()
-                .filter(s -> s.getShares() > 100.0)
-                .forEach(s -> System.out.println(s.getName()));
-        System.out.println("== The following stocks are sorted by shares:");
-        Comparator<Stock> byShares = Comparator.comparing(Stock::getShares);
-        Stream<Stock> sortedByShares = myStocks.stream()
-                .sorted(byShares);
-        sortedByShares.forEach(s -> System.out.println("Stock: " + s.getName() + " - Shares: " +
-                s.getShares()));
-// May or may not return a value
-        Optional<Stock> maybe = myStocks.stream()
-                .findFirst();
-        System.out.println("First Stock: " + maybe.get().getName());
-        List newStocks = new ArrayList();
-        Optional<Stock> maybeNot = newStocks.stream()
-                .findFirst();
-        Consumer<Stock> myConsumer = (s) ->
-        {
-            System.out.println("First Stock (Optional): " + s.getName());
-        };
-        maybeNot.ifPresent(myConsumer);
-        if (maybeNot.isPresent()) {
-            System.out.println(maybeNot.get().getName());
-        }
-        newStocks.add(new Stock("MCD", "McDonalds", 300.0));
-        Optional<Stock> maybeNow = newStocks.stream()
-                .findFirst();
-        maybeNow.ifPresent(myConsumer);
+        System.out.println("All Stocks Greater than 100.0 Shares? " + allGt);
     }
 }
