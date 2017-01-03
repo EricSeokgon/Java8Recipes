3c53c2ec44144749b478911b1f0e394a8d134d8c
hadeslee <hadesleeq@gmail.com>
2017-01-04 00:25:20 +0900

    Catching Exceptions

---
+++ src/chapter9/Catching.java
@@ -0,0 +1,27 @@
+package chapter9;
+
+/**
+ * Created by hadeslee on 2017-01-04.
+ */
+public class Catching {
+
+    private void start() {
+        System.out.println("Is th string 1234 longer than 5 chars?:" +
+                isStringShorterThanFiveCharacters("1234"));
+        System.out.println("Is th string 12345 longer than 5 chars?:" +
+                isStringShorterThanFiveCharacters("12345"));
+        System.out.println("Is th string 123456 longer than 5 chars?:" +
+                isStringShorterThanFiveCharacters("123456"));
+        System.out.println("Is th string null longer than 5 chars?:" +
+                isStringShorterThanFiveCharacters(null));
+    }
+
+    private boolean isStringShorterThanFiveCharacters(String aString) {
+        try {
+            return aString.length() > 5;
+        } catch (NullPointerException e) {
+            System.out.println("An Exception Occurred!");
+            return false;
+        }
+    }
+}
