73c862a6807dfd970e17702aadb3c6e30d3d5a5b
hadeslee <hadesleeq@gmail.com>
2017-01-08 06:02:51 +0900

    Creating an Exception Class

---
+++ src/chapter9/IllegalChatServerException.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+/**
+ * Created by hadeslee on 2017-01-08.
+ */
+public class IllegalChatServerException extends RuntimeException {
+    IllegalChatServerException(String message) {
+        super(message);
+    }
+}
