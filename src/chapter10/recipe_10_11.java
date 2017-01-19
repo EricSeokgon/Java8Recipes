571988fd81904615805184cb8dcd22e4ab9d1fb7
sklee <hadesleeq@gmail.com>
2017-01-19 15:41:50 +0900

    Updating a Common Value Across Multiple Threads

--- src/chapter10/recipe_10_11.java
+++ src/chapter10/recipe_10_11.java
@@ -13,32 +13,33 @@ import java.util.concurrent.atomic.DoubleAdder;
  */
 public class recipe_10_11 {
     DoubleAdder da = new DoubleAdder();
+
     public static void main(String[] args) {
 
-    private void start() {
-        DoubleAdder da = null;
-        Thread thread1 = new Thread(() -> {
-            for (int i1 = 0; i1 < 10; i1++) {
-                da.add(i1);
-                System.out.println("Adding " + i1);
+        private void start () {
+            DoubleAdder da = null;
+            Thread thread1 = new Thread(() -> {
+                for (int i1 = 0; i1 < 10; i1++) {
+                    da.add(i1);
+                    System.out.println("Adding " + i1);
+                }
+            });
+            Thread thread2 = new Thread(() -> {
+                for (int i1 = 0; i1 < 10; i1++) {
+                    da.add(i1);
+                    System.out.println("Adding " + i1);
+                }
+            });
+            thread1.start();
+            thread2.start();
+            try {
+                System.out.println("Sleep while summing....");
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-        Thread thread2 = new Thread(() -> {
-            for (int i1 = 0; i1 < 10; i1++) {
-                da.add(i1);
-                System.out.println("Adding " + i1);
-            }
-        });
-        thread1.start();
-        thread2.start();
-        try {
-            System.out.println("Sleep while summing....");
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("The sum is: " + da.doubleValue());
         }
-        System.out.println("The sum is: " + da.doubleValue());
     }
 }
-    }
 
