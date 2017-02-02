d9010c498069d1e893c2e0a54c3c6593a461f007
sklee <hadesleeq@gmail.com>
2017-02-03 08:27:25 +0900

    Performing CRUD Operations

---
+++ src/chapter13/CreateConnection.java
@@ -0,0 +1,21 @@
+package chapter13;
+
+import java.sql.Connection;
+
+/**
+ * Project: Java8Recipes
+ * FileName: CreateConnection
+ * Date: 2017-02-03
+ * Time: 오전 8:26
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateConnection {
+
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
