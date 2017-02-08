d09bad80a155d4b68520ad625551aec754400afd
sklee <hadesleeq@gmail.com>
2017-02-09 08:46:42 +0900

    Creating an Updatable ResultSet

--- src/chapter13/recipe_13_9.java
+++ src/chapter13/recipe_13_9.java
@@ -15,36 +15,33 @@ import java.sql.SQLException;
  * To change this template use File | Settings | File Templates.
  */
 public class recipe_13_9 {
-    private static String recipeNumber;
-
-    public static void main(String[] args) {
-        private static void queryAndUpdateDbRecipes (String recipeNumber){
-            String sql = "SELECT ID, RECIPE_NUMBER, RECIPE_NAME, DESCRIPTION " +
-                    "FROM RECIPES " +
-                    "WHERE RECIPE_NUMBER = ?";
-            ResultSet rs = null;
-            Connection conn = null;
-            try (PreparedStatement pstmt =
-                         conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
-                pstmt.setString(1, recipeNumber);
-                rs = pstmt.executeQuery();
-                while (rs.next()) {
-                    String desc = rs.getString(4);
-                    System.out.println("Updating row" + desc);
-                    rs.updateString(4, desc + " -- More to come");
-                    rs.updateRow();
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            } finally {
-                if (rs != null) {
-                    try {
-                        rs.close();
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
-                    }
+    private static void queryAndUpdateDbRecipes(String recipeNumber) {
+        String sql = "SELECT ID, RECIPE_NUMBER, RECIPE_NAME, DESCRIPTION " +
+                "FROM RECIPES " +
+                "WHERE RECIPE_NUMBER = ?";
+        ResultSet rs = null;
+        Connection conn = null;
+        try (PreparedStatement pstmt =
+                     conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
+            pstmt.setString(1, recipeNumber);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String desc = rs.getString(4);
+                System.out.println("Updating row" + desc);
+                rs.updateString(4, desc + " -- More to come");
+                rs.updateRow();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
                 }
             }
         }
     }
 }
+
