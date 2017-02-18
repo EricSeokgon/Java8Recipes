5a6e23191f61788b4ffdb92ed52f82e21bcd5a4f
hadeslee <hadesleeq@gmail.com>
2017-02-19 01:19:38 +0900

    Filtering Data in a RowSet

---
+++ src/chapter13/FilteredRowSetExample.java
@@ -0,0 +1,10 @@
+package chapter13;
+
+/**
+ * Created by hadeslee on 2017-02-19.
+ */
+public class FilteredRowSetExample {
+    public static Connection conn = null;
+    public static CreateConnection createConn;
+    public static FilteredRowSet frs = null;
+}
